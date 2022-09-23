21a174b74d7a438e8cca168212e49cd8334259b8
Sreesh Manickababu <sreeshmanickababu@Sreeshs-MacBook-Air.local>
2022-09-22 22:01:23 -0700

    feat: add async-sorted-merge functionality using priority queue and async/await

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,43 @@
 "use strict";
 
+const { PriorityQueue } = require("@datastructures-js/priority-queue");
+
+const logSourceQueue = new PriorityQueue((a, b) => {
+  if (a.source.date < b.source.date) {
+    return -1;
+  }
+
+  if (a.source.date >= b.source.date) {
+    return 1;
+  }
+});
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const getValue = async (logSources, printSource) => {
+  return await logSources[printSource.index].popAsync();
+}
+
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve) => {
+    //Get the head of all the log sources and insert them into the priority queue
+    await Promise.all(logSources.map((logSource, indx) => {
+      return logSource.popAsync().then((value) => {
+        if (value) {
+          logSourceQueue.enqueue({ index: indx, source: value });
+        }
+      })
+    }))
+    //Iterate through the priority queue, re-inserting the head of the log sources until drained.
+    while (!logSourceQueue.isEmpty()) {
+      const printSource = logSourceQueue.dequeue();
+      const val = await getValue(logSources, printSource);
+      if (val) {
+        logSourceQueue.enqueue({ index: printSource.index, source: val });
+      }
+      printer.print(printSource.source);
+    }
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
